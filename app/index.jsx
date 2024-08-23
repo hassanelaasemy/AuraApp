83761d3b6bc3cc95821cbd10f11f634d41647288
hassanelaasemy <elaasemyhassan01@gmail.com>
2024-08-23 17:48:55 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aura</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
+      </Link>
+    </View>
+  );
+}
